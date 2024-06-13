b3a4cd80acea48652001faada1b1794fda9a9763
basilikkk <111727553+basilikkk@users.noreply.github.com>
2024-06-13 13:26:57 +0300

    ex1-ex3

---
+++ WEEK1/DAY4/exercise1.py
@@ -0,0 +1,8 @@
+"""Write a function called display_message() that prints one sentence telling everyone what you are learning in this course.
+Call the function, and make sure the message displays correctly."""
+
+
+
+def display_message():
+    print('i have studied a lot about functions')
+display_message()
---
+++ WEEK1/DAY4/exercise3.py
@@ -0,0 +1,10 @@
+"""Write a function called describe_city() that accepts the name of a city and its country as parameters.
+The function should print a simple sentence, such as "<city> is in <country>".
+For example “Reykjavik is in Iceland”
+Give the country parameter a default value.
+Call your function."""
+
+def describe_city(city, country='Russia'):
+    print(f"{city} in {country}")
+
+describe_city('Moscow')    
