4394e93b5c806c619087bc49df6cb20bb32c2663
basilikkk <111727553+basilikkk@users.noreply.github.com>
2024-06-15 17:32:37 +0300

    challenge_part1

---
+++ WEEK1/DAY5/daily_challenge.py
@@ -0,0 +1,7 @@
+comma_sep_str=input('please enter a comma separated sequence of words ')
+list_comma_str=comma_sep_str.split(',')
+print(list_comma_str)
+ordered_list=sorted(list_comma_str)
+print(ordered_list)
+comma_sep_str_ordered=', fd '.join(ordered_list)
+print(comma_sep_str_ordered)
