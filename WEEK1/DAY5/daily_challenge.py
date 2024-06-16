a3e668d74d48a69de7cee68c6909655265e34694
basilikkk <111727553+basilikkk@users.noreply.github.com>
2024-06-16 06:39:33 +0300

    using list comprehension

--- WEEK1/DAY5/daily_challenge.py
+++ WEEK1/DAY5/daily_challenge.py
@@ -1,7 +1,13 @@
 comma_sep_str=input('please enter a comma separated sequence of words ')
 list_comma_str=comma_sep_str.split(',')
-print(list_comma_str)
 ordered_list=sorted(list_comma_str)
-print(ordered_list)
-comma_sep_str_ordered=', fd '.join(ordered_list)
+comma_sep_str_ordered=', '.join(ordered_list)
 print(comma_sep_str_ordered)
+
+#using List Comprehension
+comma_sep_str_1=input('please enter a comma separated sequence of words ')
+list_comma_str_1=[word.strip() for word in comma_sep_str_1.split()]
+ordered_list_1=sorted(list_comma_str_1)
+comma_sep_str_ordered_1=", ".join(ordered_list_1)
+print(comma_sep_str_1)
+
